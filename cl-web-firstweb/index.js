6ba4e7e102f3f6822e183644858c33472babc65e
K S RENJITH <121152890+ksrenjith047a@users.noreply.github.com>
2023-08-06 10:06:24 +0530

    Update index.js

--- cl-web-firstweb/index.js
+++ cl-web-firstweb/index.js
@@ -35,10 +35,10 @@ function favouriteMovieGenre(font) {
 
 function favouriteMode(mode) {
     if (mode === "light" || !mode) {
-        setProp('--background', "var(--light)");
+   //     setProp('--background', "var(--light)");
         setProp('--text', "var(--dark)");
     } else if (mode === "dark") {
-        setProp('--background', "var(--dark)");
+     //   setProp('--background', "var(--dark)");
         setProp('--text', "var(--light)");
     }
 }
